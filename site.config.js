17f255a548e9c8abfeca2f4a905ee7abb8de48c9
emrah sonmez <emrah.sonmez@itsbilisim.com>
2020-09-10 15:37:18 +0300

    add meta tags

---
+++ site.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  title: 'Next Template',
+  description: 'Next PWA Template',
+  siteUrl: 'https://github.com/ademilter/next-web-template',
+  googleAnalytic: 'X-XXXXXXXXXX',
+  author: {
+    email: 'emrahsonm3z@gmail.com',
+    name: 'Emrah Sönmez'
+  },
+  lang: 'tr'
+}
