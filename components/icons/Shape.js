47cd9520473d1094e5ab68607ec16503b97353bc
emrah sonmez <emrah.sonmez@itsbilisim.com>
2020-09-18 18:00:38 +0300

    started creating body template..

---
+++ components/icons/Shape.js
@@ -0,0 +1,16 @@
+import * as React from 'react'
+
+function SvgShape(props) {
+  return (
+    <svg width="1em" height="1em" viewBox="0 0 13 19" fill="none" {...props}>
+      <path
+        fillRule="evenodd"
+        clipRule="evenodd"
+        d="M0 6.333C0 2.841 2.916 0 6.5 0S13 2.841 13 6.333a6.216 6.216 0 01-.778 3.007l-5.367 9.456A.408.408 0 016.5 19a.408.408 0 01-.355-.204L.78 9.343A6.217 6.217 0 010 6.333zm3.25 0C3.25 8.08 4.708 9.5 6.5 9.5s3.25-1.42 3.25-3.167c0-1.746-1.458-3.166-3.25-3.166s-3.25 1.42-3.25 3.166z"
+        fill="currentColor"
+      />
+    </svg>
+  )
+}
+
+export default SvgShape
--- pages/index.js
+++ pages/index.js
@@ -1,33 +1,126 @@
 import PropTypes from 'prop-types'
+import cn from 'classnames'
 
-import { withTranslation } from '../i18n'
+import { withTranslation, Trans } from '../i18n'
 import Layout from '../components/layout'
+
+import { Shape } from '../components/icons'
 
 import styles from './index.module.css'
 const Homepage = ({ t }) => (
   <Layout>
-    <div className={styles.body}>
-      <p>
-        <b>
-          This example demonstrates how to shrink a navigation bar when the user starts to scroll
-          the page.
-        </b>
-      </p>
-      <p>Scroll down this frame to see the effect!</p>
-      <p>Scroll to the top to remove the effect.</p>
-      <p>
-        <b>Note:</b> We have also made the navbar responsive, resize the browser window to see the
-        effect.
-      </p>
-      <p>
-        Lorem ipsum dolor dummy text to enable scrolling, sit amet, consectetur adipiscing elit, sed
-        do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis
-        nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute
-        irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
-        Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit
-        anim id est laborum.
-      </p>
-    </div>
+    <section className={styles.section}>
+      <div className={styles.hero}>
+        <div className={cn(['container', styles.heroContent])}>
+          <div>
+            <h1 className={styles.heroTitle}>
+              <Trans components={{ 1: <b /> }}>{t('Hero.Text')}</Trans>
+            </h1>
+            <button className={styles.heroApply}>{t('Hero.Apply')}</button>
+          </div>
+        </div>
+      </div>
+    </section>
+    <section className={styles.section}>
+      <div className={cn(['container', styles.vision])}>
+        <div className={styles.visionLinks}>
+          {['Vision', 'QualityPromise', 'Values'].map((item) => (
+            <a key={item} className={styles.visionBtn}>
+              {t(item)}
+            </a>
+          ))}
+        </div>
+        <p>
+          <b>
+            Evb, ein erfolgreiches Dienstleistungsunternehmen in der Telekommunikationsbranche seit
+            2015:
+          </b>
+          Wir bieten Arbeit in fünf Standorten an. Jeder Mitarbeiter hat die freie Auswahl, je nach
+          Belieben seinen Standort für eine oder unbegrenzte Zeit zu ändern.
+        </p>
+        <p>
+          Insgesamt haben wir über 450 Mitarbeiter, die sich tagtäglich für das Unternehmen und
+          unsere Kunden einsetzen. Für uns ist es wichtig, ein positives Arbeitsklima zu schaffen
+          und unsere Motivation immer hoch zu halten. Aus diesem Grund sorgen wir immer dafür, dass
+          es dir, egal an welchem Standort, gut geht.
+        </p>
+      </div>
+    </section>
+    <section className={styles.section}>
+      <div className={styles.parallax}>
+        <div className={cn(['container', styles.parallaxContent])}>
+          <div className={styles.parallaxItem}>
+            <div>
+              <h1>5</h1>
+              <h2>STANDORTEN</h2>
+              <ul>
+                {['İzmir', 'İstanbul Europa', 'İstanbul Asien', 'Ankara', 'Antalya'].map((item) => (
+                  <li key={item}>
+                    <Shape />
+                    <span>{item}</span>
+                  </li>
+                ))}
+              </ul>
+            </div>
+          </div>
+          <div className={styles.parallaxItem}>
+            <h1>450+</h1>
+            <h2>MITARBEITER</h2>
+          </div>
+          <div className={styles.parallaxItem}>
+            <h1>1,5</h1>
+            <h2>MILLIONEN BESTANDSKUNDEN</h2>
+          </div>
+        </div>
+      </div>
+    </section>
+    <section className={styles.section}>
+      <div className={cn(['container', styles.tasks])}>
+        <div className={styles.tasksContent}>
+          <h2>Aufgaben</h2>
+          <br />
+          <p>
+            Deine Aufgaben bestehen darin, unsere <b>1,5 Millionen Bestandskunden</b> zu betreuen.
+            Da wir der
+            <b> Dienstleister unseres eigenen Energiewirtschaftsunternehmens</b> sind, wirst du
+            deine meiste Zeit damit verbringen Abschläge zu ändern, Verträge zu beenden,
+            Tarifwechsel durchzuführen und Rechnungen zu erläutern. Du brauchst aber keine Bedenken
+            zu haben, denn du hast immer jemanden bei dir, der dich unterstützt, um auch jedes
+            Kundenanliegen zu lösen. Dabei musst du aber immer serviceorientiert handeln. Bei uns
+            wird guter Kundenservice sehr wertgeschätzt.
+          </p>
+        </div>
+      </div>
+    </section>
+
+    <section className={styles.section}>
+      <div className="container">
+        <div className={styles.zzz}>
+          <div>
+            <img src="assets/yoga.jpg"></img>
+          </div>
+          <div>
+            <img src="assets/office_taco_party.jpg"></img>
+          </div>
+          <div>
+            <h2>Aufgaben</h2>
+            <br />
+            <p>
+              Deine Aufgaben bestehen darin, unsere <b>1,5 Millionen Bestandskunden</b> zu betreuen.
+              Da wir der
+              <b> Dienstleister unseres eigenen Energiewirtschaftsunternehmens</b> sind, wirst du
+              deine meiste Zeit damit verbringen Abschläge zu ändern, Verträge zu beenden,
+              Tarifwechsel durchzuführen und Rechnungen zu erläutern. Du brauchst aber keine
+              Bedenken zu haben, denn du hast immer jemanden bei dir, der dich unterstützt, um auch
+              jedes Kundenanliegen zu lösen. Dabei musst du aber immer serviceorientiert handeln.
+              Bei uns wird guter Kundenservice sehr wertgeschätzt.
+            </p>
+          </div>
+        </div>
+      </div>
+    </section>
+
+    <div style={{ height: '500px' }}></div>
   </Layout>
 )
 
