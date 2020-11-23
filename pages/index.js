557b3257de48649f4f4ef5c954ae135cd944a4aa
emrah sonmez <emrah.sonmez@itsbilisim.com>
2020-11-23 12:25:31 +0300

    loader remove from index page

--- pages/index.js
+++ pages/index.js
@@ -1,54 +1,18 @@
-import dynamic from 'next/dynamic'
 import PropTypes from 'prop-types'
 import styles from './index.module.css'
 
-import Loading from '../components/loader'
 import HeroSlider from '../container/hero-slider'
 import VisionSlider from '../container/vision-slider'
-// import Parallax from '../container/parallax'
-// import Tasks from '../container/tasks'
-// import MoreThanAJob from '../container/more-than-a-job'
-// import IntensiveTraining from '../container/intensive-training'
-// import Team from '../container/team'
-// import FaqSlider from '../container/faq-slider'
-// import Expectations from '../container/expectations'
+import Parallax from '../container/parallax'
+import Tasks from '../container/tasks'
+import MoreThanAJob from '../container/more-than-a-job'
+import IntensiveTraining from '../container/intensive-training'
+import Team from '../container/team'
+import FaqSlider from '../container/faq-slider'
+import Expectations from '../container/expectations'
 import Meta from '../components/meta'
 import { withTranslation } from '../i18n'
 import { HOST_URL } from '../constants'
-
-const Parallax = dynamic(() => import('../container/parallax'), {
-  loading: () => <Loading />,
-  ssr: false
-})
-const Tasks = dynamic(() => import('../container/tasks'), {
-  loading: () => <Loading />,
-  ssr: false
-})
-
-const MoreThanAJob = dynamic(() => import('../container/more-than-a-job'), {
-  loading: () => <Loading />,
-  ssr: false
-})
-
-const IntensiveTraining = dynamic(() => import('../container/parallax'), {
-  loading: () => <Loading />,
-  ssr: false
-})
-
-const Team = dynamic(() => import('../container/team'), {
-  loading: () => <Loading />,
-  ssr: false
-})
-
-const FaqSlider = dynamic(() => import('../container/faq-slider'), {
-  loading: () => <Loading />,
-  ssr: false
-})
-
-const Expectations = dynamic(() => import('../container/expectations'), {
-  loading: () => <Loading />,
-  ssr: false
-})
 
 const Homepage = ({ t, lang }) => (
   <>
